6f03ef65d33e855c044f8b0c89023d66601222c3
Sinofine Lotusie <i@sinofine.me>
2023-02-25 22:08:23 +0800

    split

---
+++ vector/src/main/java/im/vector/app/features/home/PlaceholderActivity.kt
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2023 New Vector Ltd
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package im.vector.app.features.home
+
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import dagger.hilt.android.AndroidEntryPoint
+import im.vector.app.R
+import im.vector.app.core.platform.VectorBaseActivity
+import im.vector.app.databinding.ActivityPlaceholderBinding
+@AndroidEntryPoint
+class PlaceholderActivity :
+        VectorBaseActivity<ActivityPlaceholderBinding>(){
+    override fun getBinding() = ActivityPlaceholderBinding.inflate(layoutInflater)
+    @SuppressLint("ResourceAsColor")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
+        window.statusBarColor = Color.TRANSPARENT
+        setContentView(R.layout.activity_placeholder)
+    }
+}
